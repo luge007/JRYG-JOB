2a848876aac194b8f1055ec8a143358f770973ef
luge <luge@jryghq.com>
2020-07-02 11:49:39 +0800

    增加枚举类型

--- dic/servername.go
+++ dic/servername.go
@@ -1,18 +1,18 @@
 package dic
 
-type serverName string
-
 const (
-	Account    serverName = "account"
-	Atm        serverName = "atm"
-	Bigdata    serverName = "bigdata"
-	Business   serverName = "business"
-	Charge     serverName = "charge"
-	Dictionary serverName = "dictionary"
-	Dispatcher serverName = "dispatcher"
-	Message    serverName = "message"
-	Resource   serverName = "resource"
-	Risk       serverName = "risk"
-	Order      serverName = "order"
-	Pay        serverName = "pay"
+	Account    string = "account"
+	Atm        string = "atm"
+	Bigdata    string = "bigdata"
+	Business   string = "business"
+	Charge     string = "charge"
+	Dictionary string = "dictionary"
+	Dispatcher string = "dispatcher"
+	Message    string = "message"
+	Resource   string = "resource"
+	Risk       string = "risk"
+	Order      string = "order"
+	Pay        string = "pay"
+	Test       string = "test"
+	Dev        string = "dev"
 )
