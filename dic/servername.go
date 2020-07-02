614f9f195e5eb085e1a97c90e485783562019e38
luge <luge@jryghq.com>
2020-07-02 11:17:51 +0800

    dic

---
+++ dic/servername.go
@@ -0,0 +1,21 @@
+package dic
+
+type serverName string
+
+const (
+	Business   serverName = "business"
+	Dispatcher serverName = "dispatcher"
+
+	Driver serverName = "driver"
+
+	Order serverName = "order"
+
+	Atm serverName = "atm"
+
+	Charge  serverName = "charge"
+	Bigdata serverName = "bigdata"
+
+	Dictionary serverName = "dictionary"
+
+	Account serverName = "account"
+)
